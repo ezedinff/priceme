f994b5ad508dd55b886b862f437e4b037c218309
J@tin <ezedin.fedlu@gmail.com>
2021-12-05 12:45:41 +0300

    clean up

--- src/hooks/useAuth.ts
+++
@@ -1,9 +0,0 @@
-// import * as React from "react";
-// import {AuthContext} from "../contexts/";
-//
-// function useAuth() {
-//     return React.useContext(AuthContext);
-// }
-//
-// export default useAuth;
-export  {}
