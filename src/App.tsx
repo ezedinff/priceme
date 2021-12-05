0cb854003143f966b882969a6e18cdd15cce2f15
J@tin <ezedin.fedlu@gmail.com>
2021-12-05 14:45:25 +0300

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import * as React from "react";
-import {
-  Link,
-  useNavigate,
-  Outlet
-} from "react-router-dom";
-import {AuthProvider, useAuthState} from "./contexts/auth/index";
+import {AuthProvider} from "./contexts/auth";
 import PriceMeRoutes from "./routes";
 
 export default function App() {
@@ -13,54 +8,4 @@ export default function App() {
         <PriceMeRoutes />
       </AuthProvider>
   );
-}
-
-function Layout() {
-  return (
-      <div>
-        <AuthStatus />
-
-        <ul>
-          <li>
-            <Link to="/">Public Page</Link>
-          </li>
-          <li>
-            <Link to="/protected">Protected Page</Link>
-          </li>
-        </ul>
-
-        <Outlet />
-      </div>
-  );
-}
-
-
-function AuthStatus() {
-  let auth = useAuthState();
-  let navigate = useNavigate();
-
-  if (!auth.user) {
-    return <p>You are not logged in.</p>;
-  }
-
-  return (
-      <p>
-        Welcome {auth.user}!{" "}
-        <button
-            onClick={() => {
-              auth.signout(() => navigate("/"));
-            }}
-        >
-          Sign out
-        </button>
-      </p>
-  );
-}
-
-function PublicPage() {
-  return <h3>Public</h3>;
-}
-
-function ProtectedPage() {
-  return <h3>Protected</h3>;
 }
